b7ebf7720b07c81a3c1111ab7412162ae0a4b508
pegeOS <pg.santana11@gmail.com>
2026-08-27 14:27:42 -0300

    adicionando pasta de express

---
+++ express atividade/app.js
@@ -0,0 +1,46 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use((req, res, next) => {
+    console.log(`Página acessada: ${req.url}`)
+    next()
+})
+
+
+const meuMiddleware = (nomeDaPagina) => {
+    return (req, res) => {
+        res.send(`Você está na página ${nomeDaPagina}`)
+    }
+}
+
+app.get('/', meuMiddleware('inicial'))
+app.get('/about', meuMiddleware('sobre'))
+app.post('/data', meuMiddleware('data'))
+app.get('/users', meuMiddleware('users'))
+
+
+app.get('/users/signin', (req, res) => {
+    res.redirect('/users/signup')
+})
+
+app.get('/users/signup', (req, res) => {
+    res.send("Digite seu nome de usuário na URL após o users/")
+})
+
+
+app.get('/users/:userid', (req, res) => {
+    const userid = req.params.userid
+    res.send(`Bem-vindo ${userid}!`)
+})
+
+
+app.use((req, res) => {
+    res.status(404).send('Página não encontrada <a href="/">Ir para a página inicial</a>')
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
